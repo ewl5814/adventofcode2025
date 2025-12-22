4c108c10c080f11c455799c7980e9df2dacd71be
ewl5814 <ledermanelijah@gmail.com>
2025-12-22 18:04:13 -0500

    finished part two and refactored

--- day1.scm
+++ day1.scm
@@ -3,48 +3,32 @@
 
 (define (test file) (open-input-file file))
 
+; return the offset associated with a line of input
 (define (get-num line) (string->number (substring line 1 (string-length line))))
 
+; return the non-modulo position after adding or subtracting a value from the input
 (define (move-curr curr line) 
-  (if (equal? (substring line 0 1) "L")
-   (- curr (get-num line))
-   (+ curr (get-num line))
-  )
-)
+  (if (equal? (substring line 0 1) "L") 
+    (- curr (get-num line)) 
+    (+ curr (get-num line))))
 
+; calculate the amount of times 0 will be passed or landed on with the next instruction
 (define (get-addend curr line)
   (if (or (= curr 0) (> (move-curr curr line) 99))
-    (truncate (/ (+ curr (get-num line)) 99))
-    (if (<= (move-curr curr line) 0)
-      (truncate (/ (+ (- 100 curr) (get-num line)) 100))
-      0
-    )
-))
+    (truncate (/ (+ curr (get-num line)) 100))
+    (if (<= (move-curr curr line) 0) 
+      (truncate (/ (+ (- 100 curr) (get-num line)) 100)) 0)))
 
+; recurse through every line of code and add the amount of 0 crossings
 (define (decode in curr)
   (let ((line (read-line in)))
-   (if (eof-object? line)
-    (begin (close-input-port in) 0)
-    (begin
-      (if (and (not (= curr 0)) (> (get-addend curr line) 1))
-       (begin
-        (display "curr ")
-        (display curr)
-        (newline)
-        (display line)
-        (newline)
-        (display "adding ") 
-        (display (get-addend curr line))
-        (newline)
-        (display "new curr ")
-        (display (modulo (move-curr curr line) 100))
-        (newline)
-       )
-      )
-      (+ 
-       (get-addend curr line)
-       (decode in (modulo (move-curr curr line) 100))
-      )
-))))
+   (if (eof-object? line) 0 (+ (get-addend curr line) 
+       (decode in (modulo (move-curr curr line) 100))))))
 
-(define (decode-file file) (display (decode (open-input-file file) 50)))
+; open file and decode
+(define (decode-file file)
+  (let ((in (open-input-file file))) 
+    (begin
+      (display (decode in 50))
+      (close-input-port in)
+      (newline))))
