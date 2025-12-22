1fefff43dcc1eb3be5f13a01eb50c3caed20af52
ewl5814 <ledermanelijah@gmail.com>
2025-12-22 00:00:02 -0500

    part 2 doesnt work

--- day1.scm
+++ day1.scm
@@ -3,18 +3,42 @@
 
 (define (test file) (open-input-file file))
 
-(define (move_curr curr line) 
+(define (get-num line) (string->number (substring line 1 (string-length line))))
+
+(define (move-curr curr line) 
   (if (equal? (substring line 0 1) "L")
-   (modulo (- curr (string->number (substring line 1 (string-length line)))) 100)
-   (modulo (+ curr (string->number (substring line 1 (string-length line)))) 100)
+   (- curr (get-num line))
+   (+ curr (get-num line))
   )
 )
+
+(define (get-addend curr line)
+      (if (or (and (<= (move-curr curr line) 0) (not(= 0 curr))) (> (move-curr curr line) 99))
+	(if (> (abs(- (get-num line) curr)) 99) 
+	  (truncate (/ (abs(- (get-num line) curr)) 99))
+	  1)
+	0)
+  )
 
 (define (decode in curr)
   (let ((line (read-line in)))
    (if (eof-object? line)
     (begin (close-input-port in) 0)
-    (+ (if (= 0 curr) 1 0) (decode in (move_curr curr line)))
+    (begin
+      (display "curr ")
+      (display curr)
+      (newline)
+      (display line)
+      (newline)
+      (display "adding ") 
+      (display (get-addend curr line))
+(newline)
+      (+ 
+
+     (get-addend curr line)  
+     (decode in (modulo (move-curr curr line) 100))
+      )
+    )
    )
   )
 )
