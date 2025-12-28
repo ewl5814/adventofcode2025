239a2dc41d5ff02b5776d7bd2b5123f683110e86
ewl5814 <ledermanelijah@gmail.com>
2025-12-28 15:19:48 -0500

    day 3 part 2 complete

--- day3.scm
+++ day3.scm
@@ -16,26 +16,26 @@
 	   (string->number (substring original curr-index (+ 1 curr-index))))
  
 	(find-largest-digit original tail large-index end)
-     
+
 	(find-largest-digit original tail curr-index end))))))
 
 (define (greatest-bank line)
   (let* ((tens-index (find-largest-digit line (string-tail line 1) 0 1)) 
 	 (ones-index (find-largest-digit line (string-tail line (+ tens-index 1)) (+ tens-index 1) 0)
 	))
-    (begin 
-     (display "tens: ")
-     (display tens-index)
-     (newline)
-     (display "ones: ")
-     (display ones-index)
-     (newline)
      ( string->number (string-append (substring line tens-index (+ 1 tens-index)) 
-					(substring line ones-index (+ 1 ones-index)))))))
+					(substring line ones-index (+ 1 ones-index))))))
+
+(define (greatest-twelve line prev-index end)
+  (if (= end 0)
+    ""
+   (let ((digit-index (find-largest-digit line (string-tail line (+ 1 prev-index)) (+ 1 prev-index) (- end 1)))) 
+      (string-append (substring line digit-index (+ 1 digit-index))
+		   (greatest-twelve line digit-index (- end 1))))))
 
 (define (sum-batteries in)
   (let ((line (read-line in))) 
-    (if (eof-object? line) 0 (+ (greatest-bank line)
+    (if (eof-object? line) 0 (+ (string->number(greatest-twelve line -1 12))
 	(sum-batteries in)))))
 
 (define (solve-file file)
