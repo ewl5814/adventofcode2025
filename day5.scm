77ab143fd8d2c5be01ee54fde02f2da95cd7b367
ewl5814 <ledermanelijah@gmail.com>
2026-01-01 17:36:48 -0500

    day 5 part 2 quicksort

--- day5.scm
+++ day5.scm
@@ -25,16 +25,60 @@
         (if (and (>= num (string->number (string-head range split))) (<= num (string->number (string-tail range (+ 1 split))))) 1 
           (check-in-range ranges num))))) (read-line ranges)))
 
-
 (define (search-ranges available file)
   (let ((line (read-line available)))
     (if (eof-object? line) 0
       (+ (check-in-range (open-input-file file) (string->number line))
       (search-ranges available file)))))
 
-(define (solve-file file)
+(define (count-fresh num file biggest)
+  (if (> num biggest) 0 
+  (+ (begin (check-in-range (open-input-file file) num))
+     (count-fresh (+ 1 num) file biggest))))
+
+(define (find-fresh-available file)
   (let ((available (move-to-available (open-input-file file))))
    (begin
     (display (search-ranges available file))
     (newline)
     (close-input-port available))))
+
+(define (find-biggest in large)
+    (let* ((range (read-line in)) (split (string-find-next-char range #\-)) (next-tail (string->number (string-tail range (+ 1 split)))))
+      (if (string=? range "")
+        large
+        (if (> next-tail large)
+          (find-biggest in next-tail)
+          (find-biggest in large)))))
+
+; found this swap solution online
+(define (swap-index idx1 idx2 lst)
+  (define (build-list lst idx e1 e2)
+    (cond ((null? lst)
+           '())
+          ((= idx idx1)
+           (cons e2 (build-list (cdr lst) (add1 idx) e1 e2)))
+          ((= idx idx2)
+           (cons e1 (build-list (cdr lst) (add1 idx) e1 e2)))
+          (else
+           (cons (car lst) (build-list (cdr lst) (add1 idx) e1 e2)))))
+  (build-list lst 0 (list-ref lst idx1) (list-ref lst idx2)))
+
+(define (look-for-swaps ranges low pivot i k)
+    (if (<= i low) (cons k ranges)
+    (if (> (list-ref ranges i) pivot)
+      (look-for-swaps (swap-index i k ranges) low pivot (- i 1) (- k 1))
+      (look-for-swaps ranges low pivot (- i 1) k))))
+
+(define (partition ranges low high)
+  (let* ((ranges-and-k (look-for-swaps ranges low (list-ref ranges low) high high)) (k (car ranges-and-k)) (swapped-ranges (cdr ranges-and-k)))
+    (cons k (swap-index low k swapped-ranges)))) 
+
+(define (quick-sort ranges low high)
+   (if (>= low high) ranges
+    (let* ((swapped-and-pi (partition ranges low high)) (pi (car swapped-and-pi)) (swapped (cdr swapped-and-pi)))
+     (quick-sort (quick-sort swapped low (- pi 1)) (+ pi 1) high))))
+
+(define (find-fresh file)
+  (let ((biggest (find-biggest (open-input-file file) 0)))
+    (display (count-fresh 100000000000000 file biggest))))
